b438b8c2d22e9a59c2e41f054fd0c01acfc97b43
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-09-12 09:35:18 +0530

    Added NOVA UI Banner

--- build/tools/banner.sh
+++ build/tools/banner.sh
@@ -6,16 +6,9 @@ blu='\033[0;34m'             #  blue
 ppl='\033[0;35m'             #  purple
 cya='\033[0;36m'             #  cyan
 
-echo -e ${blu}"=========================================================================="
-echo -e ${ylw}"* ╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮╱╱╭╮                                            "
-echo -e ${ylw}"* ┃╭━╮┃╱╱╱╱╭╮╱╱╱╱╭╯╰╮┃╭━╮┃╱╱┃┃                                            "
-echo -e ${ylw}"* ┃╰━╯┣━┳━━╋╋━━┳━┻╮╭╯┃╰━━┳━━┫┃╭┳╮╭┳━┳━━╮                                  "
-echo -e ${ylw}"* ┃╭━━┫╭┫╭╮┣┫┃━┫╭━┫┃╱╰━━╮┃╭╮┃╰╯┫┃┃┃╭┫╭╮┃                                  "
-echo -e ${ylw}"* ┃┃╱╱┃┃┃╰╯┃┃┃━┫╰━┫╰╮┃╰━╯┃╭╮┃╭╮┫╰╯┃┃┃╭╮┃                                  "
-echo -e ${ylw}"* ╰╯╱╱╰╯╰━━┫┣━━┻━━┻━╯╰━━━┻╯╰┻╯╰┻━━┻╯╰╯╰╯                                  "
-echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╭╯┃    BY LORDSHENRON                                           "
-echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╰━╯                                                             "
-echo -e ${cya}"* THANK YOU FOR BUILDING PROJECT SAKURA                                   "
-echo -e ${blu}"=========================================================================="
-echo -e ${cya}"You can execute the createjson.sh in vendor/lineage/build/tools to generate an OTA json for pushing OTA updates."
+echo -e ${ylw}"=========================================================================="
+echo -e ${red}"THE MOST POWERFULL CUSTOM ROM"
+echo -e ${blu}"|| NOVA UI ||"
+echo -e ${grn}"THANK YOU FOR BUILDING NOVA UI"
+echo -e ${ylw}"=========================================================================="
 
