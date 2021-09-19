8a72382c11c945b308d9fde55cc194adadb4ac64
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-09-19 14:05:03 +0530

    Fixup

--- config/common.mk
+++ config/common.mk
@@ -7,8 +7,6 @@ PRODUCT_BRAND ?= Nova_UI
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-NOVA_CODENAME := The Beginning
-
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
